d880fc3722ff1bf0d66de97b20deeb80b53d3576
wzn <997402405@qq.com>
2018-02-08 15:52:45 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
+import axios from 'axios'
+import qs from 'qs'
+const  baseURL = ''
+axios.defaults.baseURL = baseURL
+
+// 添加请求拦截器
+axios.interceptors.request.use((config) => {
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+
+//  导出api
 export default api = {
-    
+    post: (url, data) => {
+        return axios({
+            method: 'post',
+            url,
+            data: qs.stringify(data)
+        })
+    },
+    get: (url, params) => {
+        return axios({
+            method: 'get',
+            url,
+            params
+        })
+    },
+    create: (url, config = {}) => {
+        return axios.create(Object.assign({
+            url
+        }, config))
+    },
+    all: (...args, callback) => {
+        return axios.all(args).then(res => {
+            axios.spread((acct, perms) => {
+                callback && callback(acct, perms)
+            })
+        })
+    }
 }
